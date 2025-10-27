98209cd78e46b56b674d991c6f7e416b2f695a0f
breadlofe <breadlofe@gmail.com>
2025-10-26 22:46:44 -0400

    actor critic with test example

--- actor_critic.py
+++ actor_critic.py
@@ -1,9 +1,83 @@
 import os
-import tensorflow.keras as keras
-from tensorflow.keras.layers import Dense
+import tensorflow as tf
+import keras
+from keras.layers import Dense
+from keras.optimizers import Adam
+import tensorflow_probability as tfp
 
 class ActorCriticNetwork(keras.Model):
     def __init__(self, n_actions, fc1_dims=1024, fc2_dims=512,
                  name='actor_critic', chkpt_dir='tmp/actor/critic'):
         super(ActorCriticNetwork, self).__init__()
-        
+        self.fc1_dims = fc1_dims
+        self.fc2_dims = fc2_dims
+        self.n_actions = n_actions
+        self.model_name = name
+        self.checkpoint_dir = chkpt_dir
+        self.checkpoint_file = os.path.join(self.checkpoint_dir, name+'_ac')
+
+        self.fc1 = Dense(self.fc1_dims, activation='relu')
+        self.fc2 = Dense(self.fc2_dims, activation='relu')
+        self.v = Dense(1, activation=None)
+        self.pi = Dense(n_actions, activation='softmax')
+
+    def call(self, state):
+        value = self.fc1(state)
+        value = self.fc2(value)
+
+        v = self.v(value)
+        pi = self.pi(value)
+
+        return v, pi
+    
+class Agent:
+    def __init__(self, alpha=0.0003, gamma=0.99, n_actions=2):
+        self.gamma = gamma
+        self.n_actions = n_actions
+        self.action = None
+        self.action_space = [i for i in range(self.n_actions)]
+
+        self.actor_critic = ActorCriticNetwork(n_actions=n_actions)
+
+        self.actor_critic.compile(optimizer=Adam(learning_rate=alpha))
+
+    def choose_action(self, observation):
+        state = tf.convert_to_tensor([observation], dtype=tf.float32)
+        _, probs = self.actor_critic(state)
+
+        action_probabilities = tfp.distributions.Categorical(probs=probs)
+        action = action_probabilities.sample()
+        self.action = action
+
+        return action.numpy()[0]
+    
+    def save_models(self):
+        print("saving models...")
+        self.actor_critic.save_weights(self.actor_critic.checkpoint_file)
+
+    def load_models(self):
+        print("loading models...")
+        self.actor_critic.load_weights(self.actor_critic.checkpoint_file)
+
+    def learn(self, state, reward, new_state, done):
+        state = tf.convert_to_tensor([state], dtype=tf.float32)
+        new_state = tf.convert_to_tensor([new_state], dtype=tf.float32)
+        reward = tf.convert_to_tensor(reward, dtype=tf.float32)
+
+        with tf.GradientTape(persistent=True) as tape:
+            state_value, probs = self.actor_critic(state)
+            new_state_value, _ = self.actor_critic(new_state)
+            state_value = tf.squeeze(state_value)
+            new_state_value = tf.squeeze(new_state_value)
+
+            action_probs = tfp.distributions.Categorical(probs=probs)
+            log_prob = action_probs.log_prob(self.action)
+
+            delta = reward + self.gamma*new_state_value*(1-int(done)) - state_value
+            actor_loss = -log_prob*delta
+            critic_loss = delta**2
+
+            total_loss = actor_loss + critic_loss
+
+        gradient = tape.gradient(total_loss, self.actor_critic.trainable_variables)
+        self.actor_critic.optimizer.apply_gradients( zip(gradient, self.actor_critic.trainable_variables) )
